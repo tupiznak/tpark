f0906734ae52418c9f90ebb67fc46cdaf6d8ef86
Tolya <a.k.soldatov@mail.ru>
2016-03-16 01:27:31 +0300

    update 3_2 edjudge status OK still need optimisation kostili :D

--- Algorithms/3_2/3_2.cpp
+++ Algorithms/3_2/3_2.cpp
@@ -22,9 +22,14 @@ int32_t find_max(int32_t *array, int32_t N){
 	int32_t middle = (left + right) / 2;
 	
 	if (right == 0) // всего 1 элемент в массиве
-		return middle;
+		return right;
+		
 	do{ 
 		if (middle == N - 1) // дошли до конца массива
+			return middle;
+		if (right == 1) // for example for array -1 4
+			return ((array[0] > array[1])?0:1);
+		if (middle == 0)
 			return middle;
 		if ((array[middle] > array[middle+1]) && (array[middle] > array[middle-1])) // нашли максимум
 			return middle;
@@ -33,7 +38,7 @@ int32_t find_max(int32_t *array, int32_t N){
 		else  // идем вправо
 			left = middle + 1;
 		middle = (left + right) / 2; 	
-	}while( middle > 0);
+	}while( !(left >= right));
 	
 	return middle;
 }
