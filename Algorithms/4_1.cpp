9b91d57742319628aa0ac168ef37fa5f99cd01ac
iP1ng <a.k.soldatov@mail.ru>
2016-03-17 08:42:52 +0400

    4_1, 6_2
    edjudge status OK

---
+++ Algorithms/4_1.cpp
@@ -0,0 +1,99 @@
+﻿/***********************************************************************
+* 					Soldatov A.K. APO-13                               *
+*						Task 4_1                                       *
+***********************************************************************/
+#include <iostream>
+
+class CQueue {
+public:
+	CQueue(int size);
+	~CQueue() { delete[] buffer; }
+	void Enqueue(int a);
+	int Dequeue();
+private:
+	int* buffer;
+	int bufferSize;
+	int head; // Указывает на первый элемент очереди.
+	int tail; // Указывает на следующий после последнего.
+	int realSize; // количество реально занятых элементов в buffer
+};
+
+CQueue::CQueue(int size) : // инициализация переменных в конструкторе
+bufferSize(size),
+head(0),
+tail(0),
+realSize(0)
+{
+	buffer = new int[bufferSize]; // Создаем буфер.
+}
+// Добавление элемента.
+void CQueue::Enqueue(int a)
+{
+	if ( realSize == bufferSize)		// расширять буффер нужно только если все элементы в буфере заняты
+	{
+		int *temp = new int[bufferSize<<1];
+		int tp = head;
+		for (int i = (bufferSize << 1) - (bufferSize - head), j = 0; i < (bufferSize << 1); ++i, ++j) // копируем старые элементы в новый массив:
+		{
+			temp[i] = buffer[head+j];																	// все, что после head,- вправо
+		}
+		head = (bufferSize << 1) - (bufferSize - head);
+		for (int i = 0; i < head; ++i)
+		{
+			temp[i] = buffer[i];																		// все, что до head,- влево
+		}
+		tail = tp;
+		delete[] buffer;
+		bufferSize <<= 1;
+		buffer = temp;
+	} 
+	buffer[tail] = a;
+	tail = (tail + 1) % bufferSize;																		// очередь зацикливается
+	realSize++;
+}
+// Извлечение элемента.
+int CQueue::Dequeue()
+{
+	if (realSize)
+	{
+		int result = buffer[head];
+		head = (head + 1) % bufferSize;		// очередь зацикливается
+		realSize--;
+		return result;
+	}
+	else return -1;
+}
+
+int main()
+{
+	int counter = 0;
+	int curr = 0;
+	std::cin >> counter;
+	CQueue q(2);
+	for (int i = 0; i < (counter<<1); ++i)
+	{
+		std::cin >> curr;
+		switch (curr)
+		{
+		case 3:
+			std::cin >> curr;
+			q.Enqueue(curr);
+			i++;
+			break;
+		case 2:
+			std::cin >> curr;
+			if (q.Dequeue() == curr)
+			{
+				i++;
+				continue;
+			}
+			else
+			{
+				std::cout << "NO";
+				return 0;
+			}
+		}
+	}
+	std::cout << "YES";
+	return 0;
+}
---
+++ Algorithms/6_2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+long long answer(int val, long long **matrix, int height, bool stop)						
+{														
+	long long  res = 0;
+	for (int i = 0; i <= val; ++i)
+	{
+		if (height == 1 && (val==1 || val ==2))
+			return 1;
+		else if (height >= val && stop)
+			return 0;
+
+		if (matrix[val][height] != -1 && stop)
+		{
+			return matrix[val][height];
+		}
+		res += answer(val - height, matrix, height, true) + answer(val - height, matrix, height - 1, true);
+		if (stop)
+		{
+			if (matrix[val][height] == -1)
+				matrix[val][height] = res;
+			return res;
+		}
+		height++;
+	}
+	return res;
+}
+
+
+int main()
+{
+	int value = 0;
+	long long ans = 0;
+	std::cin >> value;
+	long long **matrix = new long long*[value + 1];
+	for (int i = 0; i < value + 1; ++i)
+		matrix[i] = new long long[value + 1]; 
+	for (int i = 0; i < value + 1; ++i)
+	for (int j = 0; j < value + 1; ++j)
+	{
+		matrix[i][j] = -1;
+	}
+	for (int i = 0; i < value + 1; ++i)
+	{
+		matrix[i][0] = 0;
+		matrix[0][i] = 0;
+	}
+	ans = answer(value, matrix, 1,false);
+	std::cout << ans;
+	for (int i = 0; i < value + 1; i++)
+		delete matrix[i];
+	delete[] matrix;
+	return 0;
+}
